9fd68df5d88835d615278e59c1adeb6e8cdbba0d
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-03-03 22:48:05 +0530

    securing private components

---
+++ client/src/routes/PrivateRoute.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+import { useAuth } from "../context/auth.jsx";
+import { Outlet, useNavigate, useLocation } from "react-router-dom";
+import axios from "axios";
+import Spinner from "../components/Spinner.jsx";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const PrivateRoute = () => {
+    const [ok, setOk] = useState(false);
+    // eslint-disable-next-line no-unused-vars
+    const [auth, setAuth, LogOut, isContextLoading] = useAuth();
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const authCheck = async () => {
+        try {
+            // console.log("authCheck called");
+            const res = await axios.get("http://localhost:4000/api/v1/auth/user-auth", {
+                headers: {
+                    Authorization: auth?.token,
+                },
+            });
+            // console.log("isContextLoading:" + isContextLoading);
+
+            res.data.ok ? setOk(true) : setOk(false);
+            // console.log("func:" + ok);
+        } catch (error) {
+            console.log(error);
+
+            if (error.response.status === 401 && !isContextLoading) {
+                //check for authentication when context gets updated from cookie
+                setTimeout(() => {
+                    toast.error("Please Log in to access Details!", {
+                        toastId: "userNotLoggedIn",
+                    });
+                    navigate("/login", {
+                        state: location.pathname,
+                    });
+                }, 500);
+            }
+        }
+    };
+    useEffect(() => {
+        !isContextLoading && authCheck();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [isContextLoading]);
+
+    return ok ? <Outlet /> : <Spinner />;
+};
+
+export default PrivateRoute;
