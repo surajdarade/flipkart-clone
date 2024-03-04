1f5db72f885f4639cd602a3c6e62e5aa664e4380
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-03-04 19:48:03 +0530

    router cart context auth context react toastify container provider

---
+++ client/src/main.jsx
@@ -0,0 +1,19 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { AuthProvider } from "./context/auth.jsx";
+import { CartProvider } from "./context/cart.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <AuthProvider>
+        <CartProvider>
+            <ToastContainer />
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </CartProvider>
+    </AuthProvider>
+);
